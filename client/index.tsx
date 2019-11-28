e4067dfba1e089c5a30b0e395e043050c5fe7a40
Ivan Tsai <oscar60310@gmail.com>
2019-11-29 00:22:14 +0800

    init

---
+++ client/index.tsx
@@ -0,0 +1,7 @@
+/*global document */
+import * as React from 'react';
+import { render } from 'react-dom';
+
+const app = document.getElementById('app');
+render(<div>1234</div>, app);
+// Service worker register
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable no-undef */
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.resolve(__dirname, 'client'),
+  entry: {
+    client: './index'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    plugins: [new TsconfigPathsPlugin()]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: { transpileOnly: true }
+        }
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /.svg$/],
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
+      }
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: './[name].[hash:8].js'
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './index.html',
+      filename: './index.html'
+    }),
+    new webpack.DefinePlugin({
+      ENV: JSON.stringify({
+        name: process.env.NODE_ENV
+      })
+    })
+  ],
+  devServer: {
+    hot: true
+  }
+};
