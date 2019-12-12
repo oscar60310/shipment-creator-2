dcf84f2b76c36f09b28a119f465fc987fe940e7f
Ivan Tsai <oscar60310@gmail.com>
2019-12-12 10:55:44 +0000

    move customer selector to utilities

--- client/components/reports/index.tsx
+++ client/components/reports/index.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Divider } from '@blueprintjs/core';
+import MonthPicker from '../../utilities/monthPicker';
 
 const Reports = () => {
   return (
     <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
       <h2 className="bp3-heading">報表</h2>
+      <MonthPicker onSelect={console.log} />
       <Divider />
     </div>
   );
