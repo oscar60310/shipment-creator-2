3576423ad6c5beaf1bbfbf861128f335450d0323
Ivan Tsai <oscar60310@gmail.com>
2019-12-31 12:46:19 +0000

    add company name at report

--- client/components/reports/withCustomerInfo.tsx
+++ client/components/reports/withCustomerInfo.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { ReportGeneratorProps } from './reportModel';
+import { GET_SYSTEM_INFO } from '../../queries/info';
 import dayjs from 'dayjs';
+import { useQuery } from '@apollo/react-hooks';
+import { systemInfo } from '../../generated/systemInfo';
+import { Spinner } from '@blueprintjs/core';
 
 const WithCustomerInfo = (
   props: ReportGeneratorProps & { children: any; total: number }
 ) => {
   const { month, children, customer, total } = props;
+  const { data: config, loading } = useQuery<systemInfo>(GET_SYSTEM_INFO);
+  if (loading) return <Spinner />;
+
   return (
     <>
       <h2 style={{ textAlign: 'center', width: '100%' }}>
-        {month.getFullYear()} 年 {month.getMonth() + 1} 月月結單
+        <span>{config.systemInfo.companyName}</span>
+        <span style={{ margin: 5 }}>
+          {month.getFullYear()} 年 {month.getMonth() + 1} 月
+        </span>
+        <span>月結單</span>
       </h2>
       <h2 style={{ textAlign: 'center', width: '100%' }}>{customer.name}</h2>
       {children}
